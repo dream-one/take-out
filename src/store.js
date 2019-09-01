4d5d121e0f016b0e3c5b4f5213851906b522b7ab
dream-one <1092687599@qq.com>
2019-09-01 19:22:47 +0800

    add

--- src/store.js
+++ src/store.js
@@ -13,18 +13,23 @@ export default new Vuex.Store({
     shops:[]//商家列表
 
   },
+
+  // 别的组件通过this.$store.commit(方法名调用)
+  //第二个参数传递一个对象
   mutations: {//同步处理state中的数据
-    re_address(){//接受地址信息
+    re_address(state){//接受地址信息
 
     },
-    re_categorys(){//接受分类数组
+    re_categorys(state){//接受分类数组
 
     },
-    re_shops(){//接受商家数组
+    re_shops(state){//接受商家数组
 
     }
   },
-  actions: {
-
+  actions: {//异步处理
+    //示例
+    // text(具有store实例相同属性的对象){}
+    //别的组件通过dispatch(方法名)调用
   }
 })
